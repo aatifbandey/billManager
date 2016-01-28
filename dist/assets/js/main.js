73a87ffd3fdd420a13174c4b0ca5af9633066686
aatifbandey <aatifbandey2009@gmail.com>
2016-01-28 18:11:03 +0530

    final js

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -33612,22 +33612,10 @@ module.exports = appConstants;
 
 },{}],217:[function(require,module,exports){
 var resourceConstants = {
-	BIN : 'Bin',
-	SELECTED : 'Selected',
-	UNSELECTED:"Unselected",
-	SYS_IDLE : 'System is Idle',
-	CURR_SLOT: 'CURRENT SLOT',
-	VIEW_MORE: 'View More',
-	TBL_HEADER: 'Box Serial Numbers',
-	NO_RECONCILE: 'No Items To Reconcile',
-	YES_RECONCILE:'List Of Items To Reconcile',
 	FRIEND_NAME :'Friend Name',
 	TOTALBILL : 'Total bill',
 	DESCRIPTION :'Description',
-	BILL_NO : 'Bill Number',
-	PASSWORD : 'Password',
-	CLIENTCODE_001 : 'CLIENTCODE_001',
-	CLIENTCODE_002 : 'CLIENTCODE_002'
+	BILL_NO : 'Bill Number'
 };
 module.exports = resourceConstants;
 
